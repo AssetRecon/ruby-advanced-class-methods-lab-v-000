ce18a89765d1c1c59820e5574e626483d3dbf761
Edsel Caprice <caprice@mail.usf.edu>
2018-06-30 03:01:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
     if @@all.any? {|song| song.name == song_name} == true
       self.find_by_name(song_name)
     else
-      create_by_name(song_name)
+      self.create_by_name(song_name)
     end
     end
 
