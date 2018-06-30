abf1d87d0ac80eeea2df185793ccf12b8984f955
Edsel Caprice <caprice@mail.usf.edu>
2018-06-30 02:23:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   end
 
   def self.find_by_name(song_name)
-      @@all.each do |song|
+      @@all.any? do |song|
         if song.name == song_name
           found = song
         end
